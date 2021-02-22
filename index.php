60d7dd341999577c2c014062fd02e6c6a896022c
vaibhav patel <data_vaibhav1929@users.noreply.github.com>
2021-02-22 21:49:05 +0530

    Home page

---
+++ index.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>QUIZKepeer</title>
+  <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/css/material-dashboard.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</head>
+
+<body class="bg-white">
+    <div class="container text-center bg-white mt-5">
+        <div class="row justify-content-center bg-white">
+            <div class="col-md-6 col-sm-6 col-xs-12 mt-2 text-center">
+                <h1><strong>Host or attempt the quiz with ease!</strong></h1>
+                <div class="card">
+                <div class="card-header card-header-success">
+                  <h4 class="card-title">Signup</h4>
+                  <p class="card-category">Fill the following details</p>
+                </div>
+                <div class="card-body">
+                  <form>
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Name</label>
+                          <input type="text" class="form-control">
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Email</label>
+                          <input type="Email" class="form-control">
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Phone</label>
+                          <input type="tel" class="form-control">
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Password</label>
+                          <input type="password" class="form-control">
+                        </div>
+                      </div>
+                      <div class="col-md-12 text-center">
+                        <div class="form-group">
+                          <input type="submit" value="Create Account" class="btn btn-success">
+                        </div>
+                      </div>
+                    </div>
+                   </form> 
+                </div>   
+                </div>
+
+
+            </div>    
+            <div class="col-md-6 col-sm-6 col-xs-12 bg-white">
+                <img width="450" height="450" src="assets/img/Exams_header_animated.gif"/>
+            </div>
+        </div>    
+    </div>
+</body>
+
+</html>

bb712f0abe88fc7f410b904f8f73bfb62c73dfc2
Yash Shah <49yash@gmail.com>
2021-03-03 21:01:26 +0530

    text question and checkbox question added

--- QuestionsRender.php
+++ QuestionsRender.php
@@ -1,6 +1,6 @@
 <?php
 
-    function renderMcq($questions, &$start){
+    function renderRadioQuestions($questions, &$start){
 
         foreach ($questions as $key=>$value) {
          ?>
@@ -16,7 +16,7 @@
                     <div class="col-md-12 col-sm-12 col-xs-12">
                         <div class="radio">
                             <label class="fs-5 text-dark">
-                                <input type="radio" class="fs-2" name="<?php echo $key;?>">
+                                <input type="radio" class="fs-2" name="<?php echo $key;?>" value="<?php echo $optionvalue;?>">
                                 <?php echo $optionvalue;?>
                             </label>
                         </div>
@@ -25,6 +25,57 @@
             </div>
 
         </div>
-    <?php } } ?>
+    <?php } } 
+    
+    function renderCheckBoxQuestions($questions, &$start){
+
+        foreach ($questions as $key=>$value) {
+         ?>
+        <div class="card p-2 col-md-6 col-xs-12 col-sm-6">
+
+            <div class="card-header card-header-info p-2">
+                <div class="question_badge pl-2 pr-2 pt-2 h-100 fs-5">Q.<?php echo $start; $start++;?></div>
+                <div class="card-title ml-5 text-lowercase fs-5 fw-bold"><?php echo $value['question']; ?></div>
+            </div>
+
+            <div class="row m-3">
+                <?php foreach ($value['option'] as $optionvalue) { ?>
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="checkbox">
+                            <label class="fs-5 text-dark">
+                                <input type="checkbox" class="fs-2" name="<?php echo $key;?>" value="<?php echo $optionvalue;?>">
+                                <?php echo $optionvalue;?>
+                            </label>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+
+        </div>
+    <?php } } 
+    
+    function renderTextQuestions($questions, &$start){
+
+        foreach ($questions as $key=>$value) {
+         ?>
+        <div class="card p-2 col-md-6 col-xs-12 col-sm-6">
+
+            <div class="card-header card-header-info p-2">
+                <div class="question_badge pl-2 pr-2 pt-2 h-100 fs-5">Q.<?php echo $start; $start++;?></div>
+                <div class="card-title ml-5 text-lowercase fs-5 fw-bold"><?php echo $value['question']; ?></div>
+            </div>
+
+            <div class="row m-3 justify-content-center">
+                    <div class="col-md-11 col-sm-11 col-xs-11">
+                        <div class="">
+                                <input type="text" class="fs-5 text-dark form-control" name="<?php echo $key;?>">
+                        </div>
+                    </div>
+            </div>
+
+        </div>
+    <?php } } 
+
+?>
 
 
--- hostQuiz.php
+++ hostQuiz.php
@@ -5,13 +5,26 @@
 ?>
 <div class="content ml-4 mr-4">
         <?php
-        $start = 1;
-        $qnsans = array(1=>array('question'=>"What is bootstrap?",
-                                'option'=>array("UI kit", "UI framework", "UI fake", "For Lazy devs.")),
-                        2=>array('question'=>"What is CSS?",
-                                 'option'=>array("JS", "CSS", "SV", "Yash"))
-                        );
-        renderMcq($qnsans, $start);
+                $start = 1;
+                $qnsans = array(1=>array('question'=>"What is bootstrap?",
+                                        'option'=>array("UI kit", "UI framework", "UI fake", "For Lazy devs.")),
+                                2=>array('question'=>"What is CSS?",
+                                                'option'=>array("JS", "CSS", "SV", "Yash")));
+                renderRadioQuestions($qnsans, $start);
+
+                $qnsans = array(3=>array('question'=>"QNS 1 desc?",
+                                        'option'=>array("A","B","C")),
+                                4=>array('question'=>"QNS 2 desc?",
+                                                'option'=>array("SP")));
+
+                renderCheckBoxQuestions($qnsans, $start);
+
+                $qns = array(5=>array("question"=>"Kya aapke toothpaste me namak he?"));
+                renderTextQuestions($qns,$start);
+                
+                $qns = array(6=>array("question"=>"Who is god?"));
+                renderTextQuestions($qns,$start);
+
         ?>
 </div>
 </div> <!--END OF main-panel class-->

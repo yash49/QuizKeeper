ca9ea899113bd929f97f2813d4b5a2ef4a3edcc9
Yash Shah <49yash@gmail.com>
2021-03-04 21:11:30 +0530

    questions are also added to DB

--- backend/QuizSave.php
+++ backend/QuizSave.php
@@ -1,4 +1,150 @@
 <?php
+
+    function getQuestionTypeInt($s)
+    {
+        $a = array("radio"=>3,"checkbox"=>2,"loose_text"=>1,"strict_text"=>0);
+        return $a[$s];
+    }
+
+    // returns id of inserted qns
+    function insertRadioQuestion($question,$conn)
+    {
+        $qns = $question['questoion'];
+        $options = $question['options'];
+        $correctans = $question['answer'];
+        $sql="INSERT INTO MCQ (qns,options,correctans) VALUES ('".$qns."','".$options."','".$correctans."'); SELECT LAST_INSERT_ID() as yoo;";
+
+        $response = -1;
+
+        if ($conn -> multi_query($sql)) 
+        {
+            do 
+            {
+
+                if ($result = $conn -> store_result()) 
+                {
+                    while ($row = $result -> fetch_row()) 
+                    {
+                        $response = $row[0];
+                    }
+                    $result -> free_result();
+                }
+
+            }while ($conn -> next_result());
+        }
+
+        return $response;
+    }
+
+    // returns id of inserted qns
+    function insertCheckboxQuestion($question,$conn)
+    {
+        $qns = $question['questoion'];
+        $options = $question['options'];
+        $correctans = $question['answer'];
+        $sql="INSERT INTO CheckboxQns (qns,options,correctans) VALUES ('".$qns."','".$options."','".$correctans."'); SELECT LAST_INSERT_ID() as yoo;";
+
+        $response = -1;
+
+        if ($conn -> multi_query($sql)) 
+        {
+            do 
+            {
+
+                if ($result = $conn -> store_result()) 
+                {
+                    while ($row = $result -> fetch_row()) 
+                    {
+                        $response = $row[0];
+                    }
+                    $result -> free_result();
+                }
+
+            }while ($conn -> next_result());
+        }
+
+        return $response;
+    }
+
+    // returns id of inserted qns
+    function insertTextQuestion($question,$conn)
+    {
+        $qns = $question['questoion'];
+        $type = getQuestionTypeInt($question['type']);
+        $ans = $question['answer'];
+
+        $sql="INSERT INTO TextQns (type,qns,ans) VALUES (".$type.",'".$qns."','".$ans."'); SELECT LAST_INSERT_ID() as yoo;";
+
+        $response = -1;
+
+        if ($conn -> multi_query($sql)) 
+        {
+            do 
+            {
+
+                if ($result = $conn -> store_result()) 
+                {
+                    while ($row = $result -> fetch_row()) 
+                    {
+                        $response = $row[0];
+                    }
+                    $result -> free_result();
+                }
+
+            }while ($conn -> next_result());
+        }
+
+        return $response;
+    }
+
+    
+
+    // returns xid of the Question | xid is primary key
+    function insertParticularQuestionToQuiz($question,$conn)
+    {
+        if($question['type']=="radio")
+        {
+            return insertRadioQuestion($question,$conn);
+        }
+        if($question['type']=="checkbox")
+        {
+            return insertRadioQuestion($question,$conn);
+        }
+        else if($question["type"]=="loose_text" || $question["type"]=="strict_text")
+        {
+            return insertTextQuestion($question,$conn);
+        }
+        return -1;
+    }
+
+    // returns status that it was successful or it
+    function addQuestionToQuiz($qid,$question,$conn)
+    {
+        // Insert into particular table and get that xid based on type
+        $xid = insertParticularQuestionToQuiz($question,$conn);        
+
+        if($xid==-1)
+        {
+            return 0;
+        }
+
+        $type = getQuestionTypeInt($question['type']);
+        $marks = $question['mark'];
+        
+        $stmt = $conn->prepare("INSERT INTO Questions(type,xid,marks,qid)VALUES (?, ?, ?, ?)");
+
+        $stmt->bind_param("iiii",$type,$xid,$marks,$qid);
+        
+
+        if ($stmt->execute() === TRUE) 
+        {
+            return 1;
+        } 
+        else 
+        {
+            return 0;
+        }
+    }
     
     function getLatestQid($uid)
     {
@@ -64,7 +210,7 @@
 
     }
 
-    $mandatory=explode(",","quiz_title,quiz_desc,quiz_start_date,quiz_end_date,quiz_shuffle");
+    $mandatory=explode(",","quiz_title,quiz_desc,quiz_start_date,quiz_end_date,quiz_shuffle,questionCount,questionsData");
     
     $allareset = 1;
 
@@ -130,9 +276,44 @@
         }
 
         // Here I have the $qid of the user , now i Need to insert the questions 
-
         
+        // Question Structure
+        
+        // var aaa = {questionCount:2, questionsData:[
+        //     {
+        //         id:1,
+        //         question:"WHAT IS...",
+        //         answer:"ABC",
+        //         type: radio/checkbox/loose_text/strict_text
+        //         options:"CSV",
+        //         mark:10
+        //     },
+        //     {
+        //         id:2,
+        //         question:"WHAT IS...",
+        //         answer:"ABCs",
+        //         type:"text",
+        //         options:"",
+        //         mark:10
+        //     }
+        // ]};
 
+        $totalQuestions = $_POST['questionCount'];
+        $questions = $_POST['questionsData'];
+
+        for($i=0;$i<$totalQuestions;$i++)
+        {
+            $question = $questions[$i];
+            $status = addQuestionToQuiz($qid,$question,$conn);
+            if($status==0)
+            {
+                echo json_encode(array("result"=>"Failed","message"=>"Error while adding Questions"));
+                $conn->close();
+                exit("");
+            }
+        }
+
+        echo json_encode(array("result"=>"Success","message"=>"Quiz has been created successfully"));
 
         $conn->close();
         

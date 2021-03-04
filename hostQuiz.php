f8f8242e56502063a43e40daf5e0b9253c7d70cb
vaibhav patel <data_vaibhav1929@users.noreply.github.com>
2021-03-04 20:03:26 +0530

    Quiz builder 50%

--- hostQuiz.php
+++ hostQuiz.php
@@ -5,7 +5,7 @@
 ?>
 <div class="content ml-4 mr-4">
         <?php
-                $start = 1;
+                /*$start = 1;
                 $qnsans = array(1=>array('question'=>"What is bootstrap?",
                                         'option'=>array("UI kit", "UI framework", "UI fake", "For Lazy devs.")),
                                 2=>array('question'=>"What is CSS?",
@@ -23,11 +23,201 @@
                 renderTextQuestions($qns,$start);
                 
                 $qns = array(6=>array("question"=>"Who is god?"));
-                renderTextQuestions($qns,$start);
-
+                renderTextQuestions($qns,$start);*/
         ?>
+
+
+    <div class="row justify-content-center">
+        <div class="col-lg-10 col-md-10 col-sm-12">
+            <div class="card">
+                <div class="card-header card-header-tabs card-header-primary">
+                    <div class="nav-tabs-navigation">
+                        <div class="nav-tabs-wrapper">
+                            <span class="nav-tabs-title">Host QUIZ:</span>
+                            <ul class="nav nav-tabs border-0" data-tabs="tabs">
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="#quiz_details" data-toggle="tab">
+                                        <i class="material-icons">info</i> Information
+                                        <div class="ripple-container"></div>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="quiz_questions_tab" href="#quiz_questions" data-toggle="tab">
+                                        <i class="material-icons">contact_support</i> Questions
+                                        <div class="ripple-container"></div>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#quiz_participants" data-toggle="tab">
+                                        <i class="material-icons">assignment_ind</i>Participants
+                                        <div class="ripple-container"></div>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="quiz_details">
+                            <form class="row p-3" id="quiz_details_form">
+                                <div class="col-md-6 mb-4 order-md-0 order-sm-0 order-xs-0 col-sm-12">
+                                    <input type="text" required name="quiz_title" placeholder="Quiz Title"  class="form-control">
+                                </div>
+                                <div class="col-md-6 mb-4 order-md-2 order-sm-1 order-xs-1 col-sm-12">
+                                    <textarea required name="quiz_desc" placeholder="Quiz Description" class="form-control"></textarea>
+                                </div>
+
+                                <div class="col-md-6 mb-4 order-md-1 order-sm-2 order-xs-2 col-sm-12">
+                                    <input type="date" required name="quiz_start_date" class="form-control">
+                                </div>
+
+                                <div class="col-md-6 mb-4 order-md-3 order-sm-3 order-xs-3 col-sm-12">
+                                    <input type="date" required name="quiz_end_date" class="form-control">
+                                </div>
+
+
+                                <div class="radio col-md-6 order-md-4 mt-2 order-sm-4 order-xs-4 col-sm-12">
+                                        <label class="fs-5 text-dark">
+                                            <input type="checkbox" class="fs-5" name="quiz_shuffle">
+                                            Shuffle Questions?
+                                        </label>
+                                </div>
+                            </form>
+                            <div class="float-right">
+                                <button class="mr-4 btn btn-info" onclick="document.getElementById('quiz_questions_tab').click()"> Next </button>
+                            </div>
+                        </div>
+
+                        <div class="tab-pane" id="quiz_questions">
+                            <div class="row justify-content-center">
+
+                                <div class="col-md-4 col-sm-4 col-xs-4">
+                                    <div class="list-group">
+                                        <h4 class="list-group-item disabled list-group-item-action bg-info text-white">
+                                            Questions Type
+                                        </h4>
+                                        <a href="#" data-toggle="modal" data-target="#radio_modal" class="list-group-item list-group-item-action p-3">
+                                            <i class="material-icons">radio_button_checked</i> Single Choice Question (Radio buttons)
+                                        </a>
+                                        <a href="#" class="list-group-item list-group-item-action p-3">
+                                            <i class="material-icons">check_circle</i> Multiple choice Question (Checkboxes)
+                                        </a>
+                                        <a href="#" class="list-group-item list-group-item-action p-3">
+                                            <i class="material-icons">article</i> Strict Text Input (Strictly match answer)
+                                        </a>
+                                        <a href="#" class="list-group-item list-group-item-action p-3">
+                                            <i class="material-icons">assignment_turned_in</i> Loose Text Input (Manual evaluation)
+                                        </a>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div class="tab-pane" id="quiz_participants">
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--------------------------------------------------MODALS------------------------------------------------------------->
+
+    <!--------------------------------------------------Radio MODAL------------------------------------------------------------->
+
+    <div class="modal fade w-90" id="radio_modal" tabindex="-1" role="dialog" aria-labelledby="radio_modal_title" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="radio_modal_title"></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="container-fluid">
+                   <form id="radio_q_form" class="row justify-content-center">
+
+                       <div class="col-md-6 col-sm-6">
+                           <input type="text" id="radio_q_question" placeholder="Question" required class="form-control">
+                       </div>
+
+                       <div class="col-md-6 col-sm-6">
+                           <input type="number" min="2" max="15" id="radio_q_options_count" value="4" required class="form-control">
+                           <input type="button" class="btn btn-info" value="Add Options" onclick="addOptions('radio')">
+                       </div>
+
+                   </form>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 </div> <!--END OF main-panel class-->
 </div><!--END OF wrapper class-->
+
+
+<script>
+    function addOptions(type){
+
+        let handler_form = document.getElementById(type=='radio'?'radio_q_form':'check_q_form');
+
+        let count = document.getElementById(type=='radio'?"radio_q_options_count":"check_q_options_count").value;
+
+        let opTrack = handler_form.childElementCount-2;
+        for(let i = 0; i < count; i++,opTrack++){
+            let optionContainer = document.createElement("div");
+            optionContainer.name = type=='radio'?"radio_q_option":"check_q_option";
+            optionContainer.classList.add("col-md-6","col-sm-12","m-3");
+
+            let inputField = document.createElement("input");
+            inputField.id = (type=='radio'?'radio_q_option':'check_q_option')+(opTrack+1);
+            inputField.placeholder = "Option content";
+            inputField.classList.add("form-control");
+
+            let trueAnsCheck = document.createElement("input");
+            trueAnsCheck.type = "radio";
+            trueAnsCheck.name = (type=='radio'?'radio_q_option_chk':'check_q_option_chk');
+            trueAnsCheck.classList.add("radio","p-2");
+
+            optionContainer.innerHTML = "Option "+(opTrack+1)+":";
+            optionContainer.appendChild(inputField);
+            optionContainer.innerHTML += "<label>True Answer:&nbsp</label>";
+            optionContainer.appendChild(trueAnsCheck);
+
+
+            handler_form.appendChild(optionContainer);
+        }
+
+    }
+
+    /*let data = {questionCount:2, questionsData[
+            {
+                id:1
+                question:"WHAT IS..."
+                answer:"ABC"
+                type:"radio",
+                options:"CSV"
+            },
+            {
+                id:2
+                question:"WHAT IS..."
+                answer:"ABCs"
+                type:"text"
+                options:""
+            }
+        ]}*/
+</script>
 </body>
 </html>

a2aa9690a0aea7a53cb0cc11ada04fb443d4607d
vaibhav patel <data_vaibhav1929@users.noreply.github.com>
2021-03-07 18:35:47 +0530

    attempt form done

--- attemptQuiz.php
+++ attemptQuiz.php
@@ -3,17 +3,17 @@ require_once 'sidebar.php';
 renderSideBar("attemptQuiz");
 ?>
 
-<div class="content ml-3 mr-3">
+<div class="content ml-3 mr-3" >
         <div class="card ">
-            <div class="card-header card-header-primary">
-
+            <div class="card-header card-header-primary fs-4 fw-bold">
+                Quiz Credentials
             </div>
             <div class="card-body">
                 <div class="row justify-content-center">
                     <div class="col-md-8 col-sm-8 col-xs-12 justify-content-center">
                         <form action="quizkeeper/backend/AttemptQuiz.php" name="quiz_cred_form" method="post">
                             <input type="email" placeholder="Quiz Key" required  class="mr-auto form-control col-md-12 col-sm-12 col-xs-12" id="quiz_key" name="quiz_key">
-                            <input type="email" placeholder="Quiz Key" required  class="mr-auto form-control col-md-12 col-sm-12 col-xs-12" id="quiz_password" name="quiz_password">
+                            <input type="email" placeholder="Quiz Password" required  class="mr-auto mt-2 form-control col-md-12 col-sm-12 col-xs-12" id="quiz_password" name="quiz_password">
                             <input type="submit" class="btn btn-sm btn-success ml-3 mt-2" id="attempt_quiz_btn" name="attempt_quiz_btn" value="Attempt Quiz">
                         </form>
                     </div>

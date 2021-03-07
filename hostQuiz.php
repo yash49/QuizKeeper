0ecb3488eac94d050cd51f42ef80ce03c8123ca0
Yash Shah <49yash@gmail.com>
2021-03-07 17:13:00 +0530

    shuffle working

--- hostQuiz.php
+++ hostQuiz.php
@@ -152,7 +152,7 @@
 
                                 <div class="radio col-md-6 order-md-4 mt-2 order-sm-4 order-xs-4 col-sm-12">
                                         <label class="fs-5 text-dark">
-                                            <input type="checkbox" checked="<?php if($editMode) echo ($quizDetails['shuffle'] == 1?true:false); ?>" class="fs-5" name="quiz_shuffle">
+                                            <input type="checkbox" <?php if($editMode) echo ($quizDetails['shuffle'] == 1?"checked":""); ?> class="fs-5" name="quiz_shuffle">
                                             Shuffle Questions?
                                         </label>
                                 </div>

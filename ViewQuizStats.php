a2c20a8265d672218cd33f3e4db749821d3f19bf
Deep_Menpara <54411124+Deep-Menpara@users.noreply.github.com>
2021-03-14 11:39:45 +0530

    Update ViewQuizStats.php

--- ViewQuizStats.php
+++ ViewQuizStats.php
@@ -1,11 +1,118 @@
 
 <?php
-    require_once 'sidebar.php';
-    renderSideBar("hostedQuizStats");
-    require_once 'backend/connector.php';
+
+require_once 'sidebar.php';
+renderSideBar("myResults");
+require_once 'backend/connector.php';
+$qid=$_POST['qid'];
+function getAttemptedUsers($qid,$conn)
+{
+    $stmt=$conn->prepare("SELECT count(QuizAttempt.qaid) as ucount FROM Quiz,QuizAttempt where QuizAttempt.qid=Quiz.qid and Quiz.qid=? GROUP BY Quiz.qid");
+    $stmt->bind_param('i',$qid);
+    if($stmt->execute()==TRUE)
+    {
+        $result = $stmt->get_result();
+        while($row=$result->fetch_assoc())
+        {
+            if ($row['ucount']>0)
+                return $row['ucount'];
+            else
+                return 0;
+        }
+        return 0;
+    }
+    else
+        return 0;
+}
+function getTotalQuestions($qid,$conn)
+{
+    $stmt=$conn->prepare("SELECT count(qnsid) as qcount FROM Questions where qid=?");
+    $stmt->bind_param('i',$qid);
+    if($stmt->execute()==TRUE)
+    {
+        $result = $stmt->get_result();
+        while($row=$result->fetch_assoc())
+        {
+            return $row['qcount'];
+        }
+        return -1;
+    }
+    else
+        return -1;
+}
+>>>>>>> Stashed changes
 ?>
 <div class="content ml-3 mr-3">
     <div class="row justify-content-center">
-        <?php echo $_POST['qid'];?>
+
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="card card-stats">
+                <div class="card-header card-header-success card-header-icon p-2">
+                    <div class="card-icon">
+                        <i class="material-icons">quiz</i>
+                    </div>
+                    <p class="card-category">Questions</p>
+                    <h3 class="card-title">
+                        <?php echo getTotalQuestions($qid,$conn); ?>
+                    </h3>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="card card-stats">
+                <div class="card-header card-header-success card-header-icon p-2">
+                    <div class="card-icon">
+                        <i class="material-icons">people</i>
+                    </div>
+                    <p class="card-category">Total Users Attempted</p>
+                    <h3 class="card-title">
+                        <?php echo getAttemptedUsers($qid,$conn); ?>
+                    </h3>
+                </div>
+            </div>
+        </div>
+
     </div>
+
+    <div class="row justify-content-center mt-5">
+        <div class="card">
+            <div class="card-header card-header-info">
+                <h4 class="card-title">Evaluation Details</h4>
+                <p class="card-category">Check and compare your answers with correct one</p>
+            </div>
+            <div class="card-body table-responsive">
+                <table class="table table-striped table-hover">
+                    <thead class="text-info">
+                    <tr><th>User name</th>
+                        <th>Email</th>
+                        <th>Marks</th>
+                    </tr></thead>
+                    <tbody>
+                    <?php
+
+                    $start = 1;
+                    foreach ($questionsDetails as $ui_question){
+
+                        ?>
+                        <td><?php echo $start; $start++; ?></td>
+                        <td><?php echo $ui_question['question']; ?></td>
+                        <td><?php echo $ui_question['true_answer']; ?></td>
+                        <td style="background:<?php echo
+                        ($ui_question['type'] != 1)?($ui_question['user_answer'] == $ui_question['true_answer'])?'#198754':'#dc3545' : '#fd7e14'?> " class="text-white"><?php echo $ui_question['user_answer']; ?></td>
+                        <td> <span class="badge <?php echo ($ui_question['obtain_mark'] == 0)?'badge-danger':'badge-success
+                                '?>"><?php echo $ui_question['obtain_mark']."/".$ui_question['mark']; ?></span> </td>
+                        </tr>
+                    <?php } ?>
+
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
 </div>

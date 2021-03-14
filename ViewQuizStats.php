af132e0919995edf13a2961445f5687ce69a75fb
vaibhav patel <data_vaibhav1929@users.noreply.github.com>
2021-03-14 10:55:30 +0530

    Update ViewQuizStats.php

--- ViewQuizStats.php
+++ ViewQuizStats.php
@@ -1,7 +1,7 @@
 
 <?php
     require_once 'sidebar.php';
-    renderSideBar("myResults");
+    renderSideBar("hostedQuizStats");
     require_once 'backend/connector.php';
 ?>
 <div class="content ml-3 mr-3">
